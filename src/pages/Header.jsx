7a9865cac8f00d019fa9179b3129c77fef871926
theusseveen22 <mateus.krauss@gmail.com>
2022-03-21 12:36:54 -0300

    Confiracao firebase

---
+++ src/pages/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import icon from '../images/icon.png'
+import '../pages/header.css'
+
+export default function Header() {
+  return (
+      <div className='header'>
+    <div className='icon-area'><img src={icon} alt="logo icon" />
+    <h5>Key People Insights</h5>
+    </div>
+    <div className='text-header-area'>
+        <h6>Teste de qualidade Kpis</h6>
+    </div>
+    </div>
+  )
+}
